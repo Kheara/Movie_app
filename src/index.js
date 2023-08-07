d20a2c2399cac0b7b6a3ed9dafadd7e50bd2051e
man895 <man895@naver.com>
2023-08-07 09:47:18 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+// <- react파일을 가져와서 React라고 사용하겠다.
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// <- react-dom파일을 가져와서 ReactDom라고 사용하겠다.
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// <- App파일을 가져와서 App라고 사용하겠다.
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +13,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
